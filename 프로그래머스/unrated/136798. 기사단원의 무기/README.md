5fd77e4871488aa0920d48df50795bea5afdba1f
dongmin132 <114303854+dongmin132@users.noreply.github.com>
2023-07-22 01:51:55 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.56 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/README.md
+++ 프로그래머스/unrated/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
 
 ### 성능 요약
 
-메모리: 82.7 MB, 시간: 4.79 ms
+메모리: 71.9 MB, 시간: 0.56 ms
 
 ### 구분
 
--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,12 +1,14 @@
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 1;
-        int[] arr = new int[number];
-        int count =1;
         for (int i = 2; i <= number; i++) {
-            for (int j = 1; j <= i/2; j++) {
+            int count = 0;
+            for (int j = 1; j <= Math.sqrt(i); j++) {
                 if (i % j == 0) {
                     count++;
+                    if (i / j != j) {
+                        count++;
+                    }
                 }
                 if (count > limit) {
                     count = power;
@@ -14,11 +16,8 @@ class Solution {
                 }
             }
            
-            answer+=count;
-            count =1;
+            answer += count;
         }
-
-
         return answer;
     }
 }
