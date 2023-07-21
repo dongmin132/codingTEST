499051a418c0a7c4f54795dd9af2efe966ecaf1d
dongmin132 <114303854+dongmin132@users.noreply.github.com>
2023-07-22 01:13:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 275.31 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,81 +1,15 @@
-import java.util.HashMap;
-
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-////import java.util.HashMap;
-////
-////class Solution {
-////    public int[] solution(int N, int[] stages) {
-////        HashMap<Integer, Double> stageFail = new HashMap<>();
-////        int totalUser = 0;
-////        int failUser=0;
-////        int stage = 1;
-////        for (int j = 0; j < N; j++) {
-////            totalUser=0;
-////            failUser=0;
-////            for (int i = 0; i < stages.length; i++) {
-////                if (stage <= stages[i]) {
-////                    totalUser++;
-////                }
-////                if (stage == stages[i]) {
-////                    failUser++;
-////                }
-////            }
-////            stageFail.put(stage, (double)failUser / totalUser);
-////            stage++;
-////        }
-////
-////        int[] answer = new int[N];
-////        for (int i = 0; i < N; i++) {
-////            double max = -1;
-////            int rKey = 0;
-////            for (int key : stageFail.keySet()){
-////                if(max < stageFail.get(key)){
-////                    max = stageFail.get(key);
-////                    rKey = key;
-////                }
-////            }
-////            answer[i] = rKey;
-////            stageFail.remove(rKey);
-////            System.out.println(answer[i]);
-////        }
-////
-////        return answer;
-////    }
-////}
-//
-//class Solution {
-//    public String solution(String[] participant, String[] completion) {
-//        String answer = "";
-//        for (int i = 0; i < participant.length; i++) {
-//            for (int j = 0; j < completion.length; j++) {
-//                if(participant[i].contains(completion[j]))
-//                    continue;
-//                else
-//            }
-//        }
-//        return answer;
-//    }
-//}
+import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String, Integer> members = new HashMap<>();
-        for (String member : participant) {
-            members.put(member, members.getOrDefault(member, 0) + 1);
-        }
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        for ( i=0; i<completion.length; i++){
 
-        for (String failMember : completion) {
-            members.put(failMember, members.get(failMember)-1);
-        }
-
-        for (String failMember : members.keySet()) {
-            if (members.get(failMember) == 1) {
-                answer = failMember;
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
             }
         }
-        return answer;
+        return participant[i];
     }
 }
