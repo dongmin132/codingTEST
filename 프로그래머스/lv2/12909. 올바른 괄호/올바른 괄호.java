0b4ea95ab81f9631fcd9a756a6b1cdfa93074666
dongmin132 <114303854+dongmin132@users.noreply.github.com>
2023-08-17 23:44:24 +0900

    [level 2] Title: 올바른 괄호, Time: 18.67 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -5,11 +5,11 @@ class Solution {
         boolean answer = true;
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {      
             if (s.charAt(i) == '(') {
                 stack.push('(');
             } else {
-                if(stack.isEmpty())
+                if(stack.isEmpty())     //줄일 수 있는 방법을 찾아보자
                     return false;
                 else
                     stack.pop();
