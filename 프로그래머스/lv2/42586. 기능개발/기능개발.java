cc26a53ae5c6035e8053ddb196a974993f23627e
Dongmin Kim <114303854+dongmin132@users.noreply.github.com>
2023-09-10 18:02:14 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,38 +1,30 @@
-import java.util.*;
-class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
+import java.util.ArrayList;
+import java.util.List;
 
-        List<Integer> list = new ArrayList<>();
+class Solution {
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
         int[] works = new int[progresses.length];
-        
-      
-        for(int i = 0; i < speeds.length; i++){
-            works[i]  = (100 - progresses[i]) / speeds[i];
+        int count = 1;
+
+        for (int i = 0; i < works.length; i++) {
+            works[i] = (100 - progresses[i]) / speeds[i];
             if ((100 - progresses[i]) % speeds[i] != 0){
                 works[i]  += 1;
             }
         }
-        
-        int x = works[0];
-        int count=1;
-        for(int i=1;i<progresses.length;i++){
 
-            if(x>=works[i]){
-                count+=1;
-            }else{
-                list.add(count);
-                count =1;
-                x= works[i];
+        int x = works[0];
+        for (int i = 1; i < progresses.length; i++) {
+            if (x >= works[i]) {
+                count++;
+            } else {
+                answer.add(count);
+                count = 1;
+                x = works[i];
             }
         }
-        list.add(count);
-        
-        int[] answer = new int[list.size()];
-
-        for (int i = 0; i <list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-
+        answer.add(count);
         return answer;
     }
 }
