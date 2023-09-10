619285c075513f09d60b7d04c8911b73bc077102
Dongmin Kim <114303854+dongmin132@users.noreply.github.com>
2023-09-10 18:13:48 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A;
+        int B;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        System.out.println(A + B);
+    }
+
+
+}
