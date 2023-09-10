25450476432a8d172a6979d35c78d284826373ca
Dongmin Kim <114303854+dongmin132@users.noreply.github.com>
2023-09-10 16:15:20 +0900

    [level 2] Title: H-Index, Time: 0.22 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        return answer;
+        Arrays.sort(citations);
+
+        int h =0;
+        int n = citations.length;
+        for (int i = 0; i < n; i++) {
+            h=n-i;
+            if(citations[i]>=h)
+                return h;
+        }
+        return h;
     }
 }
