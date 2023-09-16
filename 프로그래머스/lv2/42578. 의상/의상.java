5a665b0db1825fadf086e11205102218d36ba5bf
Dongmin Kim <114303854+dongmin132@users.noreply.github.com>
2023-09-16 18:15:39 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -3,14 +3,13 @@ import java.util.HashMap;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        int count = 0;
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < clothes.length; i++) {
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         for (String key : map.keySet()) {
-            answer *= map.get(key)+1;
+            answer *= map.get(key)+1;       //각 원소의 경우의 입지 않는걸 포함하는 +1
         }
-        return answer-1;
+        return answer-1;        //경우의 수 중 반드시 한가지는 입어야 하므로 1을 빼준다.
     }
 }
