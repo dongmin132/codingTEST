ef907256e9eede45e53992af20d90b90bf45a4fb
Dongmin Kim <114303854+dongmin132@users.noreply.github.com>
2024-08-19 00:05:37 +0900

    [level 2] Title: 피보나치 수, Time: 2.39 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int n) {
+        
         int [] answer = new int[n+1];
         answer[0] = 0;
         answer[1] = 1;
@@ -7,7 +8,8 @@ class Solution {
         for(int i = 2;i<n+1;i++) {
             answer[i] = (answer[i-1]+answer[i-2])%1234567;
         }
-        return answer[n]%1234567;
+        
+        return answer[n];
     }
     
 
