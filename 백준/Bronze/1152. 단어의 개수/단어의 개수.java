5ba82bc6729c1e2ab6e127eb7763a187e83ab31d
Dongmin Kim <114303854+dongmin132@users.noreply.github.com>
2024-08-27 09:35:51 +0900

    [Bronze II] Title: 단어의 개수, Time: 424 ms, Memory: 37996 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        if(str.equals(" ")) {
+            System.out.println(0);
+            return;
+        }
+        String[] arr = str.trim().split(" ");
+        System.out.println(arr.length);
+    }
+}
